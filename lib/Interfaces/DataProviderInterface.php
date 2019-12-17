85508bccb9acc9c0b5abadf0275666dc846d7337
Omid Rad <omid.rad@movingimage.com>
2019-12-17 13:24:18 +0100

    CT-1573_Show_video_status_after_user_upload

--- lib/Interfaces/DataProviderInterface.php
+++ lib/Interfaces/DataProviderInterface.php
@@ -4,18 +4,11 @@ namespace MovingImage\DataProvider\Interfaces;
 
 use MovingImage\DataProvider\Wrapper\Video;
 
-/**
- * Interface DataProviderInterface.
- *
- * @author Ruben Knol <ruben.knol@movingimage.com>
- */
 interface DataProviderInterface
 {
     /**
      * Retrieve a Doctrine collection object containing videos that
      * match the criteria defined in the options.
-     *
-     * @param array $options
      *
      * @return mixed
      */
@@ -23,20 +16,12 @@ interface DataProviderInterface
 
     /**
      * Get the count of how many videos getData would yield.
-     *
-     * @param array $options
-     *
-     * @return int
      */
-    public function getCount(array $options);
+    public function getCount(array $options): int;
 
     /**
      * Get a single Video, which wraps both the Video entity and
      * the EmbedCode into a single object.
-     *
-     * @param array $options
-     *
-     * @return Video
      */
-    public function getOne(array $options);
+    public function getOne(array $options): ?Video;
 }
